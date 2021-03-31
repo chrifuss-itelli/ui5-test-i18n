d3da98ef34c4c1dc7af1067cc0f361d26692f537
chrifuss-itelli <80960528+chrifuss-itelli@users.noreply.github.com>
2021-03-31 17:09:54 +0200

    Update cli.js
    Added onlyGermanAndEnglishFiles and onlyGermanAndEnglishValues flags, to only consider these files/only print their values to console.

--- cli.js
+++ cli.js
@@ -38,6 +38,17 @@ const getKeysFromPropertiesFile = function(sFilePath) {
     return aKeys;
 }
 
+
+const getKeysAndValuesFromPropertiesFile = function(sFilePath) {
+    let aKeys = [];
+	let aValues = [];
+    propertiesReader(sFilePath).each((sKey, sValue) => {
+        aKeys.push(sKey);
+		aValues.push(sValue);
+    });
+    return [aKeys, aValues];
+}
+
 /**
  * Get's a list of all properties in a given .properties file
  * @param {string} sFilePath - the path to the file
@@ -45,7 +56,7 @@ const getKeysFromPropertiesFile = function(sFilePath) {
  * @returns {boolean}
  * @private
  */
-const doesFileMissAnyProperties = function(sFilePath, aDesiredProperties) {
+const doesFileMissAnyProperties = function(sFilePath, sScanDir, otherFilePaths, aDesiredProperties, onlyGermanAndEnglishValues) {
     let aFileKeys = getKeysFromPropertiesFile(sFilePath);
     let missesAnyProperties = false;
     let iMissingKeys = 0;
@@ -59,6 +70,33 @@ const doesFileMissAnyProperties = function(sFilePath, aDesiredProperties) {
 
             missesAnyProperties = true;
             iMissingKeys++;
+			
+			if (onlyGermanAndEnglishValues == "true"){
+				otherFilePaths.forEach(otherFile => {
+					const otherFilePath = path.join(sScanDir, otherFile);
+					let aOtherFileKeys = getKeysAndValuesFromPropertiesFile(otherFilePath)[0];
+					let aOtherFileValues = getKeysAndValuesFromPropertiesFile(otherFilePath)[1];
+					const pathArray = otherFilePath.split("\\");
+					if(pathArray[pathArray.length - 1].startsWith('i18n.') || pathArray[pathArray.length - 1].startsWith('i18n_de.')){
+						if (aOtherFileKeys.includes(sDesiredKey)) {
+							let value = aOtherFileValues[aOtherFileKeys.indexOf(sDesiredKey)];
+							console.log(chalk.redBright(`Property "${sDesiredKey}" has following value in ${otherFilePath}: ${value}`));
+						}
+					}
+				})
+			} else {
+				otherFilePaths.forEach(otherFile => {
+					const otherFilePath = path.join(sScanDir, otherFile);
+					let aOtherFileKeys = getKeysAndValuesFromPropertiesFile(otherFilePath)[0];
+					let aOtherFileValues = getKeysAndValuesFromPropertiesFile(otherFilePath)[1];
+					if (aOtherFileKeys.includes(sDesiredKey)) {
+						let value = aOtherFileValues[aOtherFileKeys.indexOf(sDesiredKey)];
+						console.log(chalk.redBright(`Property "${sDesiredKey}" has following value in ${otherFilePath}: ${value}`));
+					}
+				})
+			}
+			
+			console.log('\n');
         }
     });
 
@@ -68,13 +106,20 @@ const doesFileMissAnyProperties = function(sFilePath, aDesiredProperties) {
         console.log(chalk.green(`Found ${iMissingKeys} missing properties, well done!`));
     }
 
-
     return missesAnyProperties;
 }
 
-const checkForMissingKeys = function(sScanDir) {
+const checkForMissingKeys = function(sScanDir, onlyGermanAndEnglishFiles, onlyGermanAndEnglishValues) {
 
     console.log(chalk.bold("Starting ui5-test-i18n"));
+	
+	if (typeof onlyGermanAndEnglishValues === 'undefined'){
+		onlyGermanAndEnglishValues = "false";
+	}
+	
+	if (typeof onlyGermanAndEnglishFiles === 'undefined'){
+		onlyGermanAndEnglishFiles = "false";
+	}
 
     // check that path exists
     if (!fs.existsSync(sScanDir)) {
@@ -107,9 +152,23 @@ const checkForMissingKeys = function(sScanDir) {
     let bErrorsFound = false;
     aFiles.forEach(sFile => {
         const sFilePath = path.join(sScanDir, sFile);
-        if (doesFileMissAnyProperties(sFilePath, aPorperties)) {
-            bErrorsFound = true;
-        }
+		if (onlyGermanAndEnglishFiles == "true"){
+			var pathArray = []
+			if (sFilePath.includes("\\")){
+				pathArray = sFilePath.split("\\");
+			} else {
+				pathArray = sFilePath.split("/");
+			}
+			if(pathArray[pathArray.length - 1].startsWith('i18n.') || pathArray[pathArray.length - 1].startsWith('i18n_de.')){
+				if (doesFileMissAnyProperties(sFilePath, sScanDir, aFiles, aPorperties, onlyGermanAndEnglishValues)) {
+					bErrorsFound = true;
+				}
+			}
+		} else {
+			if (doesFileMissAnyProperties(sFilePath, sScanDir, aFiles, aPorperties, onlyGermanAndEnglishValues)) {
+				bErrorsFound = true;
+			}
+		}
     });
 
     console.log("");
@@ -119,4 +178,4 @@ const checkForMissingKeys = function(sScanDir) {
 
 
 
-checkForMissingKeys(args[0]);
+checkForMissingKeys(args[0], args[1], args[2]);
